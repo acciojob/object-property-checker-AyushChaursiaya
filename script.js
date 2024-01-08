b7c1a5fe1f34d456a765475bd3b69ed90d8000a2
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-01-08 19:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
   //   write your code here
+	return key in sampleObject;
 }
+
+const sampleObject = {
+	red: "#FF0000",
+	green: "#00FF00",
+	white: "#FFFFFF"
+}
+
+console.log(hasKey("red"));
+console.log(hasKey("blue"));
+console.log(hasKey("white"));
 
 // Do not change the code below
 const key = prompt("Enter Key.");
